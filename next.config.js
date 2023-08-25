a6d264262a0ee5a8fd606ec9236de2d986c5d3aa
se-bayan-elian <bezo2024@gmail.com>
2023-08-25 05:48:59 +0300

    fix bug : import alias @ not working

---
+++ next.config.js
@@ -0,0 +1,10 @@
+const path = require('path');
+
+module.exports = {
+  webpack: (config, { isServer }) => {
+    // Add an alias for the src directory
+    config.resolve.alias['@'] = path.join(__dirname, 'src');
+    
+    return config;
+  },
+};
