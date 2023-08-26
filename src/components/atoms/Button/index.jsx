8c6a838f9ba432eb87673351aeef31f1cb14cc05
se-bayan-elian <bezo2024@gmail.com>
2023-08-26 04:10:04 +0300

    add : add some guides comments

--- src/components/atoms/Button/index.jsx
+++ src/components/atoms/Button/index.jsx
@@ -2,7 +2,9 @@ import React from 'react';
 import { StyledButton } from './style';
 
 const Button = ({ children, variant, ...props }) => {
-  // text => 
+  // variant :
+  // primary ,secondary ,warning ,success 
+  // button take width 100% , make sure to wrap it with styled wrapper
   
   return (
     <StyledButton variant={variant} {...props}>
