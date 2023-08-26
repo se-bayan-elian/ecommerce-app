7af1255a08f63f361152b4558d2a6f69112354eb
se-bayan-elian <bezo2024@gmail.com>
2023-08-26 04:10:41 +0300

    setup : checkbox atom

---
+++ src/components/atoms/CheckBox/style.js
@@ -0,0 +1,18 @@
+import styled from "styled-components";
+
+export const StyledCheckBox = styled.label`
+  display: flex;
+  align-items: center;
+  gap: 10px;
+  ${props => {
+    return `
+    color: ${props.theme.colors.dark};
+    font-family: ${props.theme.fonts.inter};
+    font-size: ${props.theme.fontSizes.md};
+    `
+  }}
+  font-style: normal;
+  font-weight: 400;
+  line-height: normal;
+
+`
