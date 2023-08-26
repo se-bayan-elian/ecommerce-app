2ed31d935f7368c7125bc0203e936fc5c0f55c1a
se-bayan-elian <bezo2024@gmail.com>
2023-08-26 04:29:12 +0300

    delete : unused atoms

--- src/components/atoms/Link/index.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import StyledLink from './style';
-
-const Link = ({ children, href }) => {
-  return <StyledLink href={href}>{children}</StyledLink>;
-};
-
-export default Link;
