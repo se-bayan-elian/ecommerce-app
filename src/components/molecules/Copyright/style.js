ad7edb0a6c38fa33dba77276a650211a4e382b40
mo7mad4 <mohammedreda814@gmail.com>
2023-08-27 23:27:56 +0300

    Fix bug : Making margin for all component

--- src/components/molecules/Copyright/style.js
+++ src/components/molecules/Copyright/style.js
@@ -2,6 +2,7 @@ import styled from "styled-components";
 
 const Style = styled.div`
         .stack{
+            margin-top:15px;
             display:flex;
             justify-content: space-between;
         }
