d1c6eac358e44db209e10bece87c54b3a1940f91
Adla Mostafa <adlamostafa422004@gmail.com>
2023-08-28 00:54:21 +0300

    Setup new mockData for the FirstCtegory Sectin

---
+++ src/components/pages/HomePage/mock/ProductsCategoryData.jsx
@@ -0,0 +1,40 @@
+export const productData = [
+    {
+      title: 'Soft chairs',
+      price: 'USD 19',
+      src: '/assets/images/furniture/03.png'
+    },
+    {
+      title: 'Soft & chairs',
+      price: 'USD 19',
+      src: '/assets/images/furniture/04.png',
+    },
+    {
+        title:'Kitchen dishes',
+        price:'USD 19',
+        src:'/assets/images/furniture/06.png' ,
+    },{
+        title:'Samrt watches',
+            price:'USD 19',
+            src:'/assets/images/other/03.png',
+    },{
+        title:'Kitchen mixer',
+        price:'USD 100',
+        src:'/assets/images/other/09.png',
+    },{
+        title:'Blenders',
+        price:'USD 39',
+        src:'/assets/images/other/08.png',
+    },
+    {
+        title:'Home appliance',
+        price:'USD 19',
+        src:'/assets/images/other/01.png',
+    },
+    {
+        title:'Coffee maker',
+        price:'USD 19',
+        src:'/assets/images/other/04.png',
+    },
+  ];
+
