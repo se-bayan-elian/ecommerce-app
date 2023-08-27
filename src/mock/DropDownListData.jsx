e358a908a54f9477e32b947cbd0b48387dca8060
sajashoaib <islamsaja08@gmail.com>
2023-08-27 19:41:52 +0300

    MockData: dropDownData

---
+++ src/mock/DropDownListData.jsx
@@ -0,0 +1,8 @@
+export const dropdownData = {
+    Category: ['Mobile accessory', 'Electronics', 'Smartphones ', 'Modern tech'],
+    Brands: ['Samsung', 'Apple', 'Huawei', 'Pocco','Lenovo'],
+    Features: ['Metallic', 'Plastic cover', '8GB Ram', 'Super power','Large Memory'],
+    priceRange:['1','2','3'],
+    Condition: ['Any', 'Refurbished', 'Brand new','Old items'],
+    Rating: ['⭐⭐⭐⭐⭐⭐', '⭐⭐⭐⭐', '⭐⭐⭐', '⭐⭐'],
+  }
