1adb9ae329302fce7294d1db85c1811fcdd5c957
mo7mad4 <mohammedreda814@gmail.com>
2023-08-27 14:22:13 +0300

    Alter footer component : Alter and create footer for all pages

--- src/components/organisms/Footer/index.jsx
+++ src/components/organisms/Footer/index.jsx
@@ -7,14 +7,8 @@ import Typography from '../../atoms/Typography'
 import fullLogo from '../../../../public/assets/images/Logo/logo.png'
 import appStore from '../../../../public/assets/images/Icon/AppStore.png'
 import googlePlay from '../../../../public/assets/images/Icon/GooglePlay.png'
-
-// icons
-import { ImFacebook } from 'react-icons/im'
-import { BsTwitter } from 'react-icons/bs'
-import { TfiLinkedin } from 'react-icons/tfi'
-import { RiInstagramFill } from 'react-icons/ri'
-import { AiFillYoutube } from 'react-icons/ai'
 import Image from 'next/image';
+import { FOOTER__DATA, ICONS } from '../../../mock/HeaderandFooterData';
 
 const Footer = () => {
     return (
@@ -22,54 +16,25 @@ const Footer = () => {
             <div className='grid__wrapper'>
                 <div className='footer__brand'>
                     <Image src={fullLogo} alt="logo" />
-                    <Typography as ='p' variant="default">Best information about the company gies here but now lorem ipsum is</Typography>
+                    <Typography as='p' variant="default">Best information about the company gies here but now lorem ipsum is</Typography>
                     <div>
-                        <a href="https://www.facebook.com/" className='social_icon'>
-                            <ImFacebook />
-                        </a>
-                        <a href="http://twitter.com/" className='social_icon'>
-                            <BsTwitter />
-                        </a>
-                        <a href="https://www.linkedin.com/" className='social_icon'>
-                            <TfiLinkedin />
-                        </a>
-                        <a href="https://www.instagram.com/" className='social_icon'>
-                            <RiInstagramFill />
-                        </a>
-                        <a href="https://www.youtube.com/" className='social_icon'>
-                            <AiFillYoutube />
-                        </a>
+                        {ICONS.map((item, index) =>
+                            <a href={item.href} className='social_icon' key={index}>
+                                {item.SOCIAL_ICON}
+                            </a>
+                        )}
                     </div>
                 </div>
                 <div className='footer__lists'>
-                    <div className='footer__list'>
-                        <Typography as="p"variant="default-muted" color=''>About</Typography >
-                        <Typography as="a" variant="default" >About Us</Typography >
-                        <Typography as="a" variant="default" >Find store</Typography >
-                        <Typography as="a" variant="default" >Categories</Typography >
-                        <Typography as="a" variant="default" >Blogs</Typography >
-                    </div>
-                     <div className='footer__list'>
-                        <Typography as="p" variant="default" >Partnership</Typography >
-                        <Typography as="a" variant="default" >About Us</Typography >
-                        <Typography as="a" variant="default" >Find store</Typography >
-                        <Typography as="a" variant="default" >Categories</Typography >
-                        <Typography as="a" variant="default" >Blogs</Typography >
-                    </div>
-                   <div className='footer__list'>
-                        <Typography  as="p" variant="default">Information</Typography >
-                        <Typography  as="a" variant="default">Help Center</Typography >
-                        <Typography  as="a" variant="default">Money Refund</Typography >
-                        <Typography  as="a" variant="default">Shipping</Typography >
-                        <Typography  as="a" variant="default">Contact us</Typography >
-                    </div>
-                      <div className='footer__list'>
-                        <Typography  as="p" variant="default">For users</Typography >
-                        <Typography  as="a" variant="default">Login</Typography >
-                        <Typography  as="a" variant="default">Register</Typography >
-                        <Typography  as="a" variant="default">Settings</Typography >
-                        <Typography  as="a" variant="default">My Orders</Typography >
-                    </div> 
+                    {FOOTER__DATA.map((item, index) =>
+                        <div className='footer__list' key={index}>
+                            <Typography as="p" variant="default-muted">{item.head}</Typography >
+                            {item.Data.map(hyperlink =>
+                                <Typography as="a" variant="default" >{hyperlink}</Typography >
+                            )}
+                        </div>
+                    )}
+
                     <div className='footer__list'>
                         <Typography as="p" variant="default" >Get app</Typography >
                         <Typography as="a" variant="default" >
