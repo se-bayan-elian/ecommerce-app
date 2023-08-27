2ae40c6eb65609231cfe3651087a51d2bc17afc3
sajashoaib <islamsaja08@gmail.com>
2023-08-27 20:06:39 +0300

    add: BreadCrumb.jsx

---
+++ src/components/pages/StoreListPage/Sections/BreadCrumb.jsx
@@ -0,0 +1,20 @@
+'use client'
+import React from 'react'
+import { StyledBreadCrumb, StyledLink } from './style'
+
+const BreadCrumb = () => {
+  return (
+    <div>
+      <StyledBreadCrumb>
+        <li><StyledLink href="/">Home</StyledLink></li>
+        <li><StyledLink href="/">Clothings</StyledLink></li>
+        <li><StyledLink href="/">Men’s wear</StyledLink></li>
+        <li><StyledLink href="/">Summer clothing</StyledLink></li>
+
+
+      </StyledBreadCrumb>
+    </div>
+  )
+}
+
+export default BreadCrumb
