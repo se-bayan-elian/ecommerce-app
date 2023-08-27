3bcc4087a753a624f383221598e5454afd5431d4
sajashoaib <islamsaja08@gmail.com>
2023-08-27 20:06:00 +0300

    add: sections.jsx

---
+++ src/components/pages/StoreListPage/Sections/index.jsx
@@ -0,0 +1,28 @@
+"use client";
+import React from "react";
+import BreadCrumb from "./BreadCrumb";
+import Container from "@/components/atoms/Container";
+import ViewOptionsProducts from "./ViewOptionsProducts";
+import ProductCardInformation from "./ProductCardInformation";
+import { StyledSec } from "./style";
+import Pagnation from "./Pagnation";
+import DropDownList from "./DropDownList";
+const Sections = () => {
+  return (
+    <div>
+      <Container>
+        <BreadCrumb />
+        <StyledSec>
+          <DropDownList/>
+          <div className="sec2">
+            <ViewOptionsProducts />
+            <ProductCardInformation />
+          </div>
+        </StyledSec>
+        <Pagnation />
+      </Container>
+    </div>
+  );
+};
+
+export default Sections;
