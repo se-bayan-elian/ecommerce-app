4401ddb333fae77dd1b5f0637bba8edcda0f912b
Adla Mostafa <adlamostafa422004@gmail.com>
2023-08-28 19:18:32 +0300

    Updata the code for the ProductsByCategorySection2 by adding image URL for the background

--- src/components/pages/HomePage/Sections/ProductsByCategorySection2/ProductsByCategorySection2.jsx
+++ src/components/pages/HomePage/Sections/ProductsByCategorySection2/ProductsByCategorySection2.jsx
@@ -2,39 +2,19 @@ import React from 'react';
 import ProductsByCategorySection from '../../Components/organism/ProductsByCategorySection';
 import {productDataSecond} from '../../mock/ProductsCategoryDataSection2'
 const ProductsByCategorySection2 = () => {
+  const doubleItemContent = {
+    title: 'Consumer\nelectronics and\ngadgets',
+    buttonText: 'Source now',
+  };
   return (
    <div>
     <ProductsByCategorySection
      productData={productDataSecond} 
-     background='/assets/images/other/Section2'/>
+     background='/assets/images/other/Section2.png'
+     doubleItemContent={doubleItemContent}
+     />
    </div>
-    
   );
 };
 
 export default ProductsByCategorySection2;
-
-
-// import React from 'react';
-// import { DoubleSizeItem, GridItem, GridParent } from './style';
-// import ProductCategoryComponent from '../../Components/molecules/ProductCategoryComponent/ProductCategoryComponent';
-// import {productData} from '../../mock/ProductsCategoryData'; // Import the mock product data
-
-// const ProductsByCategorySection2 = () => {
-//   return (
-//     <GridParent>
-//       <DoubleSizeItem>1</DoubleSizeItem>
-//       {productData.map((product, index) => (
-//         <GridItem key={index}>
-//           <ProductCategoryComponent
-//             title={product.title}
-//             price={product.price}
-//             src={product.src}
-//           />
-//         </GridItem>
-//       ))}
-//     </GridParent>
-//   );
-// };
-
-// export default ProductsByCategorySection2;
