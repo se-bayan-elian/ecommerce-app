5d0039d55b3aa32cfb842a4ec757edef5d57f768
Adla Mostafa <adlamostafa422004@gmail.com>
2023-08-28 19:19:38 +0300

    Styled For the SuppliersForm

--- src/components/pages/HomePage/Sections/SuppliersSection/style.js
+++ src/components/pages/HomePage/Sections/SuppliersSection/style.js
@@ -1,22 +1,37 @@
 import { styled } from "styled-components";
-import { lightColors } from "../../../../../constants/styles";
-
-// Import the background image
-import backgroundImage from '../../../../../../public/assets/images/other/SupplierSectionBack.png';
 
 export const StyledSuppliersSection = styled.div`
   width: 88%;
   margin: 50px 94px;
-  height: 420px;
+  height: 380px;
   top: 1406px;
-  background-image: url(${backgroundImage}); // Apply the background image
-  background-size: cover; // Adjust the background size
-  background-position: center; // Center the background image
+  background-image: url(assets/images/other/SupplierSectionBack.png); 
+  background-size: cover; 
+  background-position: center; 
   left: 130px;
   border: 1px;
-  background-color:red;
+  border-radius:10px;
+  display:flex;
+  justify-content:space-between;
   h3 {
-    color: white;
+    color:${props => props.theme.colors.whiteColor};
     font-family: 'Inter', sans-serif;
-}
+    letter-spacing: 1px; 
+    line-height: 1.4; 
+  }
+
+  h6 {
+    font-weight: 300;
+    color:${props => props.theme.colors.whiteColor};
+    font-family: 'Inter', sans-serif;
+    letter-spacing: 0.5px;
+    line-height: 1.6;
+  }
+  .FormPart{
+    margin:40px 20px;
+  }
+`;
+export const TextParent = styled.div`
+  padding: 15px;
+  margin: 10px 40px;
 `;
