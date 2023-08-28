91b6faf77b4ab46bb97bba91abf625cf0b5a2656
Adla Mostafa <adlamostafa422004@gmail.com>
2023-08-28 19:02:57 +0300

    Setup new atom Components :FormInput

---
+++ src/components/pages/HomePage/Components/atoms/FormInput/FormInput.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const FormInput = ({ placeholder }) => {
+  return(
+    <div>
+        <input type="text" placeholder={placeholder} />
+    </div>
+    ) 
+
+};
+
+export default FormInput;
