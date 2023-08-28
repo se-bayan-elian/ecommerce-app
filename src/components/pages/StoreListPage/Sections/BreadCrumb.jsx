45db3e6882be0b8e884a7229ec37dd84fa02d3d2
sajashoaib <islamsaja08@gmail.com>
2023-08-28 14:48:22 +0300

    update: formate code

--- src/components/pages/StoreListPage/Sections/BreadCrumb.jsx
+++ src/components/pages/StoreListPage/Sections/BreadCrumb.jsx
@@ -1,20 +1,26 @@
-'use client'
-import React from 'react'
-import { StyledBreadCrumb, StyledLink } from './style'
+"use client";
+import React from "react";
+import { StyledBreadCrumb, StyledLink } from "./style";
 
 const BreadCrumb = () => {
   return (
     <div>
       <StyledBreadCrumb>
-        <li><StyledLink href="/">Home</StyledLink></li>
-        <li><StyledLink href="/">Clothings</StyledLink></li>
-        <li><StyledLink href="/">Men’s wear</StyledLink></li>
-        <li><StyledLink href="/">Summer clothing</StyledLink></li>
-
-
+        <li>
+          <StyledLink href="/">Home</StyledLink>
+        </li>
+        <li>
+          <StyledLink href="/">Clothings</StyledLink>
+        </li>
+        <li>
+          <StyledLink href="/">Men’s wear</StyledLink>
+        </li>
+        <li>
+          <StyledLink href="/">Summer clothing</StyledLink>
+        </li>
       </StyledBreadCrumb>
     </div>
-  )
-}
+  );
+};
 
-export default BreadCrumb
+export default BreadCrumb;
