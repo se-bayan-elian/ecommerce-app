99030e32ecd2eb9cb4572dddd01b8f7cd7ccf2a9
Adla Mostafa <adlamostafa422004@gmail.com>
2023-08-28 19:11:42 +0300

    Adding Background image for the DoubleSizeItem

--- src/components/pages/HomePage/Components/organism/ProductsByCategorySection/index.jsx
+++ src/components/pages/HomePage/Components/organism/ProductsByCategorySection/index.jsx
@@ -2,13 +2,14 @@
 import React from 'react';
 import { DoubleSizeItem, GridItem, GridParent } from './style';
 import ProductCategoryComponent from '../../../Components/molecules/ProductCategoryComponent/ProductCategoryComponent';
-import Button from '../../../../../atoms/Button';
 
-const ProductsByCategorySection = ({ productData = [], background,variant}) => {
+const ProductsByCategorySection = ({ productData = [], background,variant,doubleItemContent}) => {
   return (
     <GridParent>
-      <DoubleSizeItem background={background}>
-        {/* <Button text='aDLA' variant={variant}/> */}
+      <DoubleSizeItem style={{backgroundImage:`url(${background})`}}>
+        <div className='DoubleItemContent'>
+        <h2>{doubleItemContent.title}</h2>
+        <button>{doubleItemContent.buttonText}</button></div>
       </DoubleSizeItem>
       {productData.map((product, index) => (
         <GridItem key={index}>
