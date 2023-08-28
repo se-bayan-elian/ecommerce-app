e55c8ce4bb01b7ff554206630470b60ed40b23ba
mo7mad4 <mohammedreda814@gmail.com>
2023-08-28 21:21:36 +0300

    Setup new component : Create code for sign up page and import at sign up form data

--- src/components/pages/SignUpPage/index.jsx
+++ src/components/pages/SignUpPage/index.jsx
@@ -1,8 +1,50 @@
-import React from 'react'
+"use client";
+import React, { useState } from 'react'
+import AuthPages from '../../layouts/AuthPages/components'
+import Style from './style'
+import SignupForm from './SignupForm';
+import  signUpSchema  from '../../../validations/signUpSchema';
+import { Body1, H3 } from './components/Typography';
+import Link from 'next/link';
+
+const initialState = {
+  name: { value: "", error: "" },
+  surname: { value: "", error: "" },
+  email: { value: "", error: "" },
+  phone: {
+    code: "",
+    number: "",
+    error: ""
+  },
+  password1: { value: "", error: "" },
+  password2: { value: "", error: "" },
+  agree: { value: false, error: "" },
+}
 
 const SignUpPage = () => {
+  const [data, setData] = useState(initialState);
+  const handelSubmit = (e) => {
+    e.preventDefault();
+    signUpSchema.validate({
+      name: data.name.value,
+      surname: data.surname.value,
+      email: data.email.value,
+      phoneCode: data.phone.code,
+      phoneNumber: data.phone.number,
+      password1: data.password1.value,
+      password2: data.password2.value,
+      agree: data.agree.value,
+    }, { abortEarly: false })
+  }
   return (
-    <div>SignUpPage</div>
+    <AuthPages>
+      <Style>
+        <H3 margin="0 0 17px">Register</H3>
+        <SignupForm {...{ data, setData, handelSubmit }} />
+        <Body1 align="center" color="gray/800">Already have an accaunt?
+          <Link href="/login" color="blue">Log in</Link></Body1>
+      </Style>
+    </AuthPages>
   )
 }
 
