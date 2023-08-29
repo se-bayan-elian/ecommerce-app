66c144c2e3a6b00b9c9f65abcb83cc58ff064482
ahmedkilany01 <ahmedkilany.prv@gmail.com>
2023-08-30 00:16:48 +0300

    create data to use in Products details page

---
+++ src/mock/info.js
@@ -0,0 +1,112 @@
+export const pics = [
+  {
+    id: 1,
+    title: "clothes1",
+    src: "/assets/images/cloth/01.png",
+  },
+  {
+    id: 2,
+    title: "clothes2",
+    src: "/assets/images/cloth/01.png",
+  },
+  {
+    id: 3,
+    title: "clothes3",
+    src: "/assets/images/cloth/01.png",
+  },
+  {
+    id: 4,
+    title: "clothes4",
+    src: "/assets/images/cloth/01.png",
+  },
+  {
+    id: 5,
+    title: "clothes5",
+    src: "/assets/images/cloth/01.png",
+  },
+  {
+    id: 6,
+    title: "clothes6",
+    src: "/assets/images/cloth/01.png",
+  },
+];
+
+export const detailsTable = [
+  {
+    id: 1,
+    title: "Model",
+    value: "#8786867",
+  },
+  {
+    id: 2,
+    title: "Style",
+    value: "Classic style",
+  },
+  {
+    id: 3,
+    title: "Certificate",
+    value: "ISO-898921212",
+  },
+  {
+    id: 4,
+    title: "Size",
+    value: "34mm x 450mm x 19mm",
+  },
+  {
+    id: 5,
+    title: "Memory",
+    value: "36GB RAM",
+  },
+];
+
+export const features = [
+  {
+    id: 1,
+    title: "Some great feature name here",
+  },
+  {
+    id: 2,
+    title: "Lorem ipsum dolor sit amet, consectetur",
+  },
+  {
+    id: 3,
+    title: "Duis aute irure dolor in reprehenderit",
+  },
+  {
+    id: 4,
+    title: "Some great feature name here",
+  },
+];
+
+export const cards = [
+  {
+    id: 1,
+    title: "Xiaomi Redmi 8 Original",
+    price: "$32.00-$40.00",
+    src: "/assets/images/tech/01.png",
+  },
+  {
+    id: 2,
+    title: "Xiaomi Redmi 8 Original",
+    price: "$32.00-$40.00",
+    src: "/assets/images/tech/01.png",
+  },
+  {
+    id: 3,
+    title: "Xiaomi Redmi 8 Original",
+    price: "$32.00-$40.00",
+    src: "/assets/images/tech/01.png",
+  },
+  {
+    id: 4,
+    title: "Xiaomi Redmi 8 Original",
+    price: "$32.00-$40.00",
+    src: "/assets/images/tech/01.png",
+  },
+  {
+    id: 5,
+    title: "Xiaomi Redmi 8 Original",
+    price: "$32.00-$40.00",
+    src: "/assets/images/tech/01.png",
+  },
+];
