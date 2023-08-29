42a64d014607d22281b81708d0e2dc982048aa45
mo7mad4 <mohammedreda814@gmail.com>
2023-08-29 13:34:24 +0300

    Fix bug :  Change img pure to Image next

--- src/components/layouts/AuthPages/components/FooterAuth/index.jsx
+++ src/components/layouts/AuthPages/components/FooterAuth/index.jsx
@@ -9,15 +9,16 @@ import visa2 from '../../../../../../public/assets/images/Icon/masterCard.png';
 import visa3 from '../../../../../../public/assets/images/Icon/paypal.png';
 import visa4 from '../../../../../../public/assets/images/Icon/visa.png';
 import Style from './style';
+import Image from 'next/image';
 
 const FooterAuth = () => {
     return (
         <Style>
             <div className='visa'>
-                <img src={visa1} alt="visa1" />
-                <img src={visa2} alt="visa2" />
-                <img src={visa3} alt="visa3" />
-                <img src={visa4} alt="visa4" />
+                <Image src={visa1} alt="visa1" />
+                <Image src={visa2} alt="visa2" />
+                <Image src={visa3} alt="visa3" />
+                <Image src={visa4} alt="visa4" />
             </div>
 
             <div className='links'>
