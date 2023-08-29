968616e3d97387aee8761a478bc9e7da91fc4007
ahmedkilany01 <ahmedkilany.prv@gmail.com>
2023-08-30 00:39:39 +0300

    Add sections to ProductDetailsPage main page

--- src/components/pages/ProductDetailsPage/index.jsx
+++ src/components/pages/ProductDetailsPage/index.jsx
@@ -1,9 +1,24 @@
-import React from 'react'
+"use client";
+
+import React from "react";
+import { styled } from "styled-components";
+import ContentMain from "./sections/ContentMain";
+import Description from "./sections/Description";
+import { lightColors } from "../../../constants/styles";
+import RelatedProducts from "./sections/RelatedProducts";
+
+const ProductDetailsPageContainer = styled.div`
+  /* background-color: ${lightColors.gray100}; */
+`;
 
 const ProductDetailsPage = () => {
   return (
-    <div>ProductDetailsPage</div>
-  )
-}
+    <ProductDetailsPageContainer>
+      <ContentMain />
+      <Description />
+      <RelatedProducts />
+    </ProductDetailsPageContainer>
+  );
+};
 
-export default ProductDetailsPage
+export default ProductDetailsPage;
