31380ab3da002ac21d4aa67acc5688319fa871e0
ahmedkilany01 <ahmedkilany.prv@gmail.com>
2023-08-30 02:39:55 +0300

    Initialize: InputWithIcon atom

---
+++ src/components/atoms/InputWithIcon/TransparentButton.js
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+
+const TransparentButton = styled.button`
+    outline: none;
+    border: none;
+    background-color: transparent;
+    display: contents;
+    cursor: pointer;
+`
+
+export default TransparentButton;
