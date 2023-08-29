4d4b3fe731c9700f10fcad56dacf70e60e466430
ahmedkilany01 <ahmedkilany.prv@gmail.com>
2023-08-30 02:32:52 +0300

    Edits: add typography to Home

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,11 +1,46 @@
 "use client";
 
-import ProductDetailsPage from "../components/pages/ProductDetailsPage";
+import Typography from "../components/atoms/Typography";
 
 export default function Home() {
   return (
     <main>
-      <ProductDetailsPage />
+      <Typography as="h1">main </Typography>
+      <Typography as="h2">main </Typography>
+      <Typography as="h3">main </Typography>
+      <Typography as="h4">main </Typography>
+      <Typography as="h5">main </Typography>
+      <Typography as="h6">main </Typography>
+      <Typography as="h1">hello</Typography>
+      <Typography as="p">saja shoaib branch</Typography>
+      <Typography as="p" variant={"default"}>
+        main{" "}
+      </Typography>
+      <Typography as="p" variant={"default-muted"}>
+        main{" "}
+      </Typography>
+      <Typography as="p" variant={"small"}>
+        main{" "}
+      </Typography>
+      <Typography as="p" variant={"small-muted"}>
+        main{" "}
+      </Typography>
+      <Typography as="p" variant={"micro"}>
+        main{" "}
+      </Typography>
+      <Typography as="p" variant={"micro-muted"}>
+        main{" "}
+      </Typography>
+
+      <Typography as="a" variant={"default-muted"} href="/hhh">
+        main{" "}
+      </Typography>
+      <Typography as="a" variant={"small-muted"} href="/hhh">
+        main{" "}
+      </Typography>
+      <Typography as="a" variant={"micro-muted"} href="/hhh">
+        main{" "}
+      </Typography>
     </main>
   );
 }
