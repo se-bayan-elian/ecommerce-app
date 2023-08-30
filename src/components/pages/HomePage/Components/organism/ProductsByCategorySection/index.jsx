97707c6f8f5a5c5a7650a8dc1b547327767dca2f
Adla Mostafa <adlamostafa422004@gmail.com>
2023-08-30 17:56:43 +0300

    wrap some line with divs

--- src/components/pages/HomePage/Components/organism/ProductsByCategorySection/index.jsx
+++ src/components/pages/HomePage/Components/organism/ProductsByCategorySection/index.jsx
@@ -1,17 +1,17 @@
-// ProductsByCategorySection.jsx
 import React from 'react';
 import { DoubleSizeItem, GridItem, GridParent } from './style';
 import ProductCategoryComponent from '../../../Components/molecules/ProductCategoryComponent/ProductCategoryComponent';
+import Typography from '../../../../../atoms/Typography';
 
-const ProductsByCategorySection = ({ productData = [], background,variant,doubleItemContent}) => {
+const ProductsByCategorySection = ({ productData = [], background,doubleItemContent}) => {
   return (
     <GridParent>
       <DoubleSizeItem style={{backgroundImage:`url(${background})`}}>
         <div className='DoubleItemContent'>
-        <h2>{doubleItemContent.title}</h2>
-        <button>{doubleItemContent.buttonText}</button></div>
+         <h2 className='doubleItem-title'>{doubleItemContent?.title}</h2>
+        <button>{doubleItemContent?.buttonText}</button></div>
       </DoubleSizeItem>
-      {productData.map((product, index) => (
+        {productData.map((product, index) => (
         <GridItem key={index}>
           <ProductCategoryComponent
             title={product.title}
@@ -20,6 +20,7 @@ const ProductsByCategorySection = ({ productData = [], background,variant,double
           />
         </GridItem>
       ))}
+      
     </GridParent>
   );
 };
