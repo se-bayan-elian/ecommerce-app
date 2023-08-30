9b383962abd0b183067657b904a0e975de53f99d
ahmedkilany01 <ahmedkilany.prv@gmail.com>
2023-08-31 02:05:27 +0300

    Update: add new mock data

--- src/mock/info.js
+++ src/mock/info.js
@@ -110,3 +110,73 @@ export const cards = [
     src: "/assets/images/tech/01.png",
   },
 ];
+
+export const trade_prices = [
+  {
+    id: 1,
+    price: 98.0,
+    quantity: "50-100",
+    is_first: true,
+    selected: true,
+  },
+  {
+    id: 2,
+    price: 90.0,
+    quantity: "100-700",
+  },
+  {
+    id: 3,
+    price: 78.0,
+    quantity: "700+",
+  },
+];
+
+export const details = [
+  {
+    id: 1,
+    content: [{ id: 1, title: "Price", detail: "Negotiable" }],
+  },
+  {
+    id: 2,
+    content: [
+      { id: 1, title: "Type", detail: "Classic shoes" },
+      { id: 2, title: "Material", detail: "Plastic material" },
+      { id: 3, title: "Design", detail: "Modern nice" },
+    ],
+  },
+  {
+    id: 3,
+    content: [
+      { id: 1, title: "Customization", detail: "Customized logo and design custom packages" },
+      { id: 2, title: "Protection", detail: "Refund Policy" },
+      { id: 3, title: "Warranty", detail: "2 years full warranty" },
+    ],
+  },
+];
+
+export const you_may_like_items = [
+  {
+    id: 1,
+    title: "Apple Watch Series Space Gray",
+    price: "$7.00 - $99.50",
+    image_src: "/assets/images/cloth/01.png",
+  },
+  {
+    id: 2,
+    title: "Apple Watch Series Space Gray",
+    price: "$7.00 - $99.50",
+    image_src: "/assets/images/cloth/01.png",
+  },
+  {
+    id: 3,
+    title: "Apple Watch Series Space Gray",
+    price: "$7.00 - $99.50",
+    image_src: "/assets/images/cloth/01.png",
+  },
+  {
+    id: 4,
+    title: "Apple Watch Series Space Gray",
+    price: "$7.00 - $99.50",
+    image_src: "/assets/images/cloth/01.png",
+  },
+];
