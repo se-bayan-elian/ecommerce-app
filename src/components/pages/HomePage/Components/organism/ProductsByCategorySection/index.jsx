2cf6f04b95d4162391767ab5a24c57ad43fa1f81
Adla Mostafa <adlamostafa422004@gmail.com>
2023-09-01 06:36:54 +0300

    Replace button by using Button from Global Components

--- src/components/pages/HomePage/Components/organism/ProductsByCategorySection/index.jsx
+++ src/components/pages/HomePage/Components/organism/ProductsByCategorySection/index.jsx
@@ -2,15 +2,15 @@ import React from 'react';
 import { DoubleSizeItem, GridItem, GridParent } from './style';
 import ProductCategoryComponent from '../../../Components/molecules/ProductCategoryComponent/ProductCategoryComponent';
 import Typography from '../../../../../atoms/Typography';
-
+import Button from '../../../../../../../src/components/atoms/Button'
 const ProductsByCategorySection = ({ productData = [], background,doubleItemContent}) => {
   return (
     <GridParent>
       <DoubleSizeItem style={{backgroundImage:`url(${background})`}}>
         <div className='DoubleItemContent'>
          <h2 className='doubleItem-title'>{doubleItemContent?.title}</h2>
-        <button>{doubleItemContent?.buttonText}</button></div>
-      </DoubleSizeItem>
+         <Button variant='secondary' >{doubleItemContent?.buttonText}</Button></div>    
+           </DoubleSizeItem>
         {productData.map((product, index) => (
         <GridItem key={index}>
           <ProductCategoryComponent
