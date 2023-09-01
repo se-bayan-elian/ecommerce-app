743e14b6afe7e93d844a710cfa2acb43ff1eae97
se-bayan-elian <bezo2024@gmail.com>
2023-09-01 04:12:24 +0300

    fix bug : convert .jsx to .js

--- src/components/pages/HomePage/mock/DealsAndOffersSectionData.js
+++ src/components/pages/HomePage/mock/DealsAndOffersSectionData.js
@@ -1,25 +1,30 @@
 export const dealsAndOffersData = [
     {
+      id : '1',
       productName: 'Samrt watches',
       src: '/assets/images/tech/03.png',
       chipLabel: '-25%',
     },
     {
+      id : '2',
       productName: 'Laptops',
       src: '/assets/images/tech/04.png',
       chipLabel: '-15%',
     },
     {
+      id : '2',
       productName: 'GoPro cameras',
       src: '/assets/images/tech/05.png',
       chipLabel: '-40%',
     },
     {
+      id : '3',
       productName: 'HeadPhones',
       src: '/assets/images/tech/06.png',
       chipLabel: '-25%',
     },
     {
+      id : '4',
       productName: 'Canon camers',
       src: '/assets/images/tech/08.png',
       chipLabel: '-25%',
