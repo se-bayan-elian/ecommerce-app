cc6dbf0bf1dbb5c2ebc70d18183d72de74d30c4c
mo7mad4 <mohammedreda814@gmail.com>
2023-09-03 21:19:29 +0300

    FIx bug: Update style

--- src/components/atoms/InputWithIcon/style.js
+++ src/components/atoms/InputWithIcon/style.js
@@ -1,26 +1,26 @@
 import styled from "styled-components";
 
 const Style = styled.div`
-  display: grid;
-  grid-template-columns: 36px 1fr;
-  align-items: center;
-  border: 1px solid ${(props) => props.theme.colors.gray300};
-  border-radius: 4px;
-
-  .icon {
-    display: flex;
+    display: grid;
+    grid-template-columns: 36px 1fr;
     align-items: center;
-    justify-content: center;
-  }
+    border: 1px solid ${props => props.theme.colors.gray300};
+    border-radius: 4px;
+    ${props => props.background ? "background-color:" + props.background + ";" : ""}
 
-  .input__subscribe {
-    border: unset;
-  
-    :focus {
-      box-shadow: unset;
-      border: unset;
+    .icon{
+        display: flex;
+        align-items: center;
+        justify-content: center;
     }
-  }
-`;
+
+    input{
+        border:unset;
+        :focus{
+            box-shadow: unset;
+            border: unset;
+        }
+    }
+`
 
 export default Style;
