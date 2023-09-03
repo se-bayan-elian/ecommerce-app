5f98596a0b2dbc9d14a31674200f51d2dbeefc24
se-bayan-elian <bezo2024@gmail.com>
2023-09-03 20:04:59 +0300

    add : not-found and loading pages styles

--- src/app/style.js
+++ src/app/style.js
@@ -29,6 +29,42 @@ export const GlobalStyle = createGlobalStyle`
   input:focus-visible{
     outline : none;
   }
+
+  /* not found page styling  */
+  .notFoundPage{
+    margin: 15% auto;
+    text-align: center;
+    .error-status{
+      color : ${props => props.theme.colors.baseColorRed};
+      margin-bottom: 10px;
+    }
+    .error-desc{
+      font-size: 1.5rem;
+      margin-bottom: 20px;
+    }
+    .error-btn{
+      display: inline-block;;
+      padding : 8px 15px;
+      background-color: ${props => props.theme.colors.baseColorRed};
+      border: 1px solid ${props => props.theme.colors.baseColorRed};
+      color : #fff;
+      border-radius : 6px;
+      transition : all 0.3s ease-in-out;
+      &:hover{
+        border-color: ${props => props.theme.colors.baseColorRed};
+        color : ${props => props.theme.colors.baseColorRed};
+        background: transparent;
+      }
+    }
+  }
+
+  /* loading page  */
+  .loading {
+    height: 100vh;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
   
   @media (max-width : ${props => props.theme.media.miniLaptop} ){
      // 1 rem = 14px for mobile and tablets
