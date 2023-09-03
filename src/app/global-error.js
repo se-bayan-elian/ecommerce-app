005ba63e6017789795bcc6ffaa9aad88c4756e16
se-bayan-elian <bezo2024@gmail.com>
2023-09-03 20:03:44 +0300

    alter : global error page

--- src/app/global-error.js
+++ src/app/global-error.js
@@ -1,11 +1,13 @@
 'use client'
 
+import Typography from "../components/atoms/Typography"
+
 export default function GlobalError({ error, reset }) {
   return (
     <html>
       <body>
-        <h2>Something went wrong!</h2>
-        <button onClick={() => reset()}>Try again</button>
+        <Typography as="h1" className="error-status">Something went wrong!</Typography>
+        <button onClick={() => reset()} className="error-btn">Try again</button>
       </body>
     </html>
   )
