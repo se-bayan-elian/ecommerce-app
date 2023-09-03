94f22deea64b352028e8ec4f8bede06691ed6b71
mo7mad4 <mohammedreda814@gmail.com>
2023-09-03 21:21:14 +0300

    Fix code : add section in home page

--- src/components/pages/HomePage/index.jsx
+++ src/components/pages/HomePage/index.jsx
@@ -3,25 +3,35 @@ import DealsAndOffersSection from './Sections/DealsAndOffersSection/DealsAndOffe
 import SuppliersSection from './Sections/SuppliersSection/SuppliersSection'
 import ProductsByCategorySection from './Components/organism/ProductsByCategorySection'
 import { productData } from './mock/ProductsCategoryData'
+import RecommendedItems from './Sections/RecommendedItems'
+import Container from '../../atoms/Container'
+import ExtraServices from './Sections/ExtraServices'
+import Suppliers from './Sections/Suppliers'
 
 const HomePage = () => {
   const doubleItemContent = {
-    section1 : {
+    section1: {
       title: 'Home and outdoor',
-       buttonText: 'Source now',
+      buttonText: 'Source now',
     },
-    section2 : {
+    section2: {
       title: 'Consumer\nelectronics and\ngadgets',
       buttonText: 'Source now'
     }
   };
   return (
     <div>
-      <DealsAndOffersSection/>
-      <ProductsByCategorySection productData={productData.section1} doubleItemContent={doubleItemContent.section1}  background='/assets/images/other/Section1.png'/>
-      <ProductsByCategorySection productData={productData.section2} doubleItemContent={doubleItemContent.section2} background='/assets/images/other/Section2.png'/>
-      
-      <SuppliersSection/>
+
+        <DealsAndOffersSection />
+        <ProductsByCategorySection productData={productData.section1} doubleItemContent={doubleItemContent.section1} background='/assets/images/other/Section1.png' />
+        <ProductsByCategorySection productData={productData.section2} doubleItemContent={doubleItemContent.section2} background='/assets/images/other/Section2.png' />
+
+        <SuppliersSection />
+      <Container>
+        <RecommendedItems />
+        <ExtraServices />
+        <Suppliers/>
+      </Container>
     </div>
   )
 }
