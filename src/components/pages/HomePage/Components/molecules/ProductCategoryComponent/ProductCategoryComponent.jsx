9de055c67b8d2144d6fb7c3cc23258b8f257c1b5
se-bayan-elian <bezo2024@gmail.com>
2023-09-03 20:17:36 +0300

    alter : wrap the product with link to show products details

--- src/components/pages/HomePage/Components/molecules/ProductCategoryComponent/ProductCategoryComponent.jsx
+++ src/components/pages/HomePage/Components/molecules/ProductCategoryComponent/ProductCategoryComponent.jsx
@@ -1,23 +1,28 @@
-import React from 'react'
-import Typography from '../../../../../atoms/Typography'
-import Image from 'next/image'
-import { StyledProductCategoryComponent } from './style'
-import { StyledP } from '../../../../../atoms/Typography/style'
-const ProductCategoryComponent = ({title,price,src}) => {
+import React from "react";
+import Typography from "../../../../../atoms/Typography";
+import Image from "next/image";
+import { StyledProductCategoryComponent } from "./style";
+import Link from "next/link";
+const ProductCategoryComponent = ({ title, price, src, href }) => {
   return (
     <StyledProductCategoryComponent>
-        <div>
-           <Typography as="h6" className="product-title">{title}</Typography>
-           <Typography as="p" variant="micro-muted" className="product-price">From <span>{price}</span></Typography>
+      <Link href={href}>
+        <div className="flex-inner">
+          <div>
+            <Typography as="h6" className="product-title">
+              {title}
+            </Typography>
+            <Typography as="p" variant="micro-muted" className="product-price">
+              From <span>{price}</span>
+            </Typography>
+          </div>
+          <div>
+            <Image src={src} width={73.948} height={57.582} />
+          </div>
         </div>
-        <div>
-            <Image src={src}
-            width={73.948}
-            height={57.582}
-             />
-        </div>
+      </Link>
     </StyledProductCategoryComponent>
-  )
-}
+  );
+};
 
-export default ProductCategoryComponent
+export default ProductCategoryComponent;
