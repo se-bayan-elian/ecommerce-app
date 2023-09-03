7d2e32e6e9ebe4ff02590f338c97f1eb218df7d5
se-bayan-elian <bezo2024@gmail.com>
2023-09-03 20:04:06 +0300

    alter : not found page

--- src/app/not-found.js
+++ src/app/not-found.js
@@ -1,11 +1,13 @@
 import Link from 'next/link'
- 
+import Typography from '../components/atoms/Typography'
+
 export default function NotFound() {
   return (
-    <div>
-      <h2>404</h2>
-      <p>Could not find requested resource</p>
-      <Link href="/">Return Home</Link>
+    <div className='notFoundPage'>
+      <Typography as="h1" align='center' className="error-status">404</Typography>
+      <Typography as="p" align='center' className="error-desc">Could not find requested resource</Typography>
+      <Link href="/" className="error-btn">Return Home</Link>
+
     </div>
   )
 }
