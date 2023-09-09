5de2b1747440804e2fe9ea2d445929976111c124
se-bayan-elian <bezo2024@gmail.com>
2023-09-09 19:04:14 +0300

    fix bug : images not shown

--- src/components/atoms/HeaderNavLink/index.jsx
+++ src/components/atoms/HeaderNavLink/index.jsx
@@ -2,11 +2,12 @@ import React from "react";
 import Link from "next/link";
 import Typography from "../Typography";
 import { StyledHeaderNavLink } from "./style";
+import Image from "next/image";
 const HeaderNavLink = ({ href, label, icon }) => {
   return (
     <StyledHeaderNavLink>
       <Link href={href}>
-        <img src={icon} alt={label} />
+        <Image src={icon} alt={label} />
         <Typography as="span" variant={"default-muted"}>
           {label}
         </Typography>
